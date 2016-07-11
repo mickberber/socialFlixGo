b072cb17964922c83d76bb632a2d80370b6fce6c
Michael Berber <mickberber@icloud.com>
2016-07-11 14:22:54 -0700

    embed socket into app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
+
+const ws = new WebSocket('ws://localhost:3000/v1/ws');
+
+ws.addEventListener('message', (e) => { console.log(e); });
+
+let style = {
+  height: '100px',
+  width: '100px'
+};
+
 export default class App extends Component {
+    clickHandler() {
+      ws.send('foo');
+    }
     render() {
-        return (<div>Hello World!</div>)
+        return (<button style={style} onClick={() => this.clickHandler()}>ping socket</button>)
     }
 }
